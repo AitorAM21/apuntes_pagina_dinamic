74ecfa11d41b97cbcb2ca6486d1aa75b34e9c23c
AitorAM21 <aitoraresmontes21@gmail.com>
2022-05-26 23:33:19 +0200

    2

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-asdsd
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('El lobo dice auu!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
